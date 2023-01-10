2eca9d89999402bad6f718508be18412f7a7f444
Zelda Ogiamien <zel0789@yahoo.com>
2023-01-09 20:55:43 -0500

    made updates to each file

--- js/index.js
+++ js/index.js
@@ -1,75 +1,66 @@
-// let allListings = [];
-
-class Zellow {
-    constructor(name, link, listingsList) {
-      this.name = name;
-      this.link = link;
-      this.listingsList = listingsList;
+class Listing{
+    constructor(name, link, listingList) {
+        this.name = name;
+        this.link = link;
+        this.listingList = listingList;
     }
     render() {
-      const li = document.createElement("li");
-  
-      const removeBtn = document.createElement("button");
-      removeBtn.textContent = "-";
-      removeBtn.addEventListener("click", () => {
-        this.bookmarkList.removeBookmark(this);
-        renderApp();
-      });
-      li.append(removeBtn);
-  
-      const a = document.createElement("a");
-      a.textContent = this.name;
-      a.href = this.link;
-      li.append(a);
-  
-      return li;
-    }
-  }
+        const li = document.createElement("li");
 
-  class ListingsList {
-    constructor(listings) {
+        const removeButton = document.createElement("button");
+        removeButton.innerText = "-";
+        removeButton.addEventListener("click", () => {
+            this.listingList.removeListing(this);
+            renderApp();
+        });
+    li.append(removeButton);
+
+    const a = document.createElement("a");
+    a.innerText = this.name;
+    a.href = this.link;
+    li.append(a);
+    
+    return li;
+    }
+} 
+
+class ListingList{
+    constructor(listings){
         this.listings = listings;
     }
-
-    addListing(listing)
+    addListing(listing){
         this.listings.push(listing);
-
-    removeListing(listing) {
-        const index = this.listings.indexOf(listing);
-        if (index !== -1) this.listings.splice(index, 1);
     }
-
     render() {
-        const ul = document.createElement("ul");
-        const renderedListings = this.listings.map((listing) => 
-            listing.render()
-        );
-        ul.replaceChildren(...renderedListings);
-        return ul;
+    // All the individual Listings need to render
+    const ul = document.createElement("ul");
+    const renderedListings = this.listings.map((listing) =>
+        listing.render()
+    );
+    ul.replaceChildren( ...renderedListings);
+    return ul;
     }
-  }
+}
 
-  const listings = new ListingsList([]);
-  const addBtn = document.querySelector(".add-listing");
-  addBtn.addEventListener("click", addListing);
+const listings = new ListingList([]);
+const addButton = document.querySelector(".add-listing");
+addButton.addEventListener("click", addListing);
 
+renderApp();
 
-  renderApp();
-
-  // create and add a listing to an array
-
-  function addListing() {
+function addListing() {
     const name = document.querySelector(".name").value;
     const link = document.querySelector(".link").value;
-
+  
+    //When I click the add button, I want it to add the listing to the listing list and then, rerender after this happens.
     const listing = new Listing(name, link, listings);
     listings.addListing(listing);
 
     renderApp();
-  }
+}
 
-    function renderApp() {
-        const main = document.querySelector("main");
-        const renderedListings = listings.render();
-        main.replaceChildren(renderedListings);
-    }
+function renderApp() {
+    const main = document.querySelector("main");
+    const renderedListings = listings.render();
+    main.replaceChildren(renderedListings);
+  }
